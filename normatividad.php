edeae178fd0d9030939da53be0961c7e87121d12
appsstars <appsstars2019@gmail.com>
2020-05-19 07:53:52 -0500

    update

--- normatividad.php
+++ normatividad.php
@@ -1,130 +1,140 @@
 <?php require_once 'layouts/header.php'; ?>
-   <div class="breadcumb-area clearfix dzsparallaxer auto-init" data-options='{direction: "normal"}'>
-        <div class="divimage dzsparallaxer--target" style="width: 101%; height: 130%; background-image: url(img/bg-img/bg-2.jpg)"></div>
-        <!-- breadcumb content -->
-        <div class="breadcumb-content">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <nav aria-label="breadcrumb" class="breadcumb--con text-center">
-                            <h2 class="w-text title wow fadeInUp" data-wow-delay="0.2s">Normatividad</h2>
-                            <ol class="breadcrumb justify-content-center wow fadeInUp" data-wow-delay="0.4s">
-                                <li class="breadcrumb-item"><a href="./">Inicio</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Normatividad</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- ##### Header Area End ##### -->
 
     <!-- ##### Welcome Area Start ##### -->
+    <div class="cabezera_servicios" data-options='{direction: "normal"}'>
+        <div class="divimage dzsparallaxer--target" style="background-image: url(img/Normatividad/normatividad.jpg);background-position:top; width:101%; height:600px; background-repeat:no-repeat"></div>
+        <div class="breadcumb-content">
 
+            <div class="container h-100">
+
+                <div class="row h-100">
+
+                    <div class="col-12" style="margin-top:350px">
+
+                        <nav aria-label="breadcrumb" class="breadcumb--con text-center">
+
+                            <h2 class="w-text title wow fadeInUp" data-wow-delay="0.2s" style="color: #ffff !important; font-weight: bold;font-size: 45px">Normatividad</h2>
+
+                            <ol class="breadcrumb justify-content-center wow fadeInUp" data-wow-delay="0.4s" >
+
+                                <li class="breadcrumb-item" ><a href="index.php" style="color: #00a8ff">Inicio</a></li>
+
+                                <li class="breadcrumb-item active" aria-current="page" style="color: #00a8ff">Servicios</li>
+
+                            </ol>
+
+                        </nav>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
     <!-- ##### Welcome Area End ##### -->
 
-
-    <div class="sh-bg">
     <!-- ##### Our Services Area Start ##### -->
 
-        <section class="contact_us_area" id="contact">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading text-center">
-                        <!-- Dream Dots -->
-                        <div class="dream-dots justify-content-center wow fadeInUp" data-wow-delay="0.2s">
-                            <span></span><span></span><span></span><span></span><span></span><span></span><span></span>
-                        </div>
-                        <h2 class="wow fadeInUp" data-wow-delay="0.3s">Normatividad</h2>
-                    </div>
-                </div>
+<section class="our_services_area section-padding-100-70 clearfix" id="normatividad">
+    <div class="container">
+        
+        <div class="section-heading text-center">
+            <!-- Dream Dots -->
+            <div class="dream-dots justify-content-center wow fadeInUp" data-wow-delay="0.2s">
+                <span></span><span></span><span></span><span></span><span></span><span></span><span></span>
             </div>
-
-            <!-- Contact Form -->
-            <div class="row justify-content-center normatividad">
-                <div class="col-12 col-md-10 col-lg-8">
-                    <div class="contact_form">
-                        <form action="#" method="post" id="main_contact_form" novalidate>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div id="success_fail_info"></div>
-                                </div>
-                                     <div class="col-md-6">
-                                        <div class="card">
-                                            <iframe src="docs/AVISO_REPROGRAMACION_PAGOS_201912060710.pdf" style="display: block; width: 100%;height: 700px;"></iframe>
-                                        </div>
-                                    </div> 
-
-                                    <div class="col-md-6">
-                                        <div class="card">
-                                            <iframe src="docs/RESOLUCION07NOV2019_201911081525.pdf" style="display: block; width: 100%;height: 700px;"></iframe>
-                                        </div>
-                                    </div> 
-
-
-                                     <div class="col-md-6">
-                                        <div class="card">
-                                            <iframe src="docs/RESOLUCION07NOV2019_201911081525.pdf" style="display: block; width: 100%;height: 700px;"></iframe>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <div class="card">
-                                            <img src="img/circular.jpeg" alt="Convocatoria" style="display: block;margin: auto;">
-                                        </div>
-                                    </div>  
-
-
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <h2 class="wow fadeInUp" data-wow-delay="0.3s">Normatividad</h2>
+            <p class="wow fadeInUp" data-wow-delay="0.4s"></p>
         </div>
-    </section>
+            
+
+        <div class="row">
+
+            <div class="col-xs-12 col-md-12">
+                <img src="img/iconos/check-square.png" class="check-square" alt=""> <a href="docs/AVISO_REPROGRAMACION_PAGOS_201912060710.pdf" target="_blank">AVISO DE PROGRAMACION  DE PAGOS RECONOCIDOS EN LA RESOLUCION No 007 DEL 08 DE NOVIEMBRE DE 2019</a>
+            </div>
+
+            <div class="col-xs-12 col-md-12">
+                <img src="img/iconos/check-square.png" class="check-square" alt="">  <a href="docs/RESOLUCION07NOV2019_201911081525.pdf" target="_blank">RESOLUCION No 007 DEL 08 DE NOVIEMBRE DE 2019</a>
+            </div>
+
+
+            <div class="col-xs-12 col-md-12">
+               <img src="img/iconos/check-square.png" class="check-square" alt="">   <a href="img/circular.jpeg" target="_blank">INFORMACION</a>
+            </div>
+
+        </div>
 
 
 
+        </div>
+    </div>
+</section>
+    <!-- ##### Our Services Area End ##### -->
 
-
-
-
-
-
+    <!-- ##### Our features Area start ##### -->
+    <section class="features section-padding-100" style="display: none;">
 
         <div class="section-heading text-center">
+            <!-- Dream Dots -->
+            <div class="dream-dots justify-content-center wow fadeInUp" data-wow-delay="0.2s">
+                <span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+            </div>
+            <h2 class="wow fadeInUp" data-wow-delay="0.3s">Our Mobile Application</h2>
+            <p class="wow fadeInUp" data-wow-delay="0.4s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.</p>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="services-column col-lg-5 col-lg-offset-1 col-md-10 offset-md-1 col-xs-10 offset-xs-1">
+                    <!--Services Block Four-->
+                    <div class="services-block-four">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <span class="icon ti-mobile"></span>
+                            </div>
+                            <h3><a href="#">Powerfull Mobile and Online App</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium modi assumenda beatae provident non hic eum dolores natus, vitae, quae, facere perferendis quas tempore. Consequuntur commodi facilis sed similique.</div>
+                            
+                        </div>
+                    </div>
+
+                    <!--Services Block Four-->
+                    <div class="services-block-four">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <span class="icon ti-widget"></span>
+                            </div>
+                            <h3><a href="#">Brings more Transparency and Speed</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aut repudiandae harum, fugit, necessitatibus veritatis molestias a eligendi distinctio. Nostrum expedita deserunt maiores adipisci.</div>
                         </div>
                     </div>
                     
                     <!--Services Block Four-->
+                    <div class="services-block-four" style="margin-bottom:0">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <span class="icon ti-settings"></span>
+                            </div>
+                            <h3><a href="#">Special for Multiple Use Capabilities</a></h3>
+                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia maiores, ducimus animi a. Nulla ab dolor doloribus, aperiam, quam dolorum dicta vitae tempora, vero at quod error alias incidunt quidem.</div>
+                        </div>
+                    </div>
                     
                 </div>
-                
+                <div class="service-img-wrapper col-lg-6 col-md-12 col-sm-12">
+                    <div class="image-box">
+                        <img src="img/phone.png" class="center-block img-responsive phone-img" alt="">
+                        <img src="img/core-img/rings-bg.png" class="center-block img-responsive rings " alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <!-- ##### Our features Area End ##### -->
-    </div>
 
-    <!-- ##### Subscribe Area start ##### -->
-    
-
-    <!-- ##### FAQ & Timeline Area End ##### -->
-
-
-    <!-- ##### Team Area Start ##### -->
-    <!-- ##### Team Area End ##### -->
-
-    
-    <!-- ##### Blog Area Start ##### -->
-   
-    <!-- ##### Blog Area End ##### -->
-
-    <!-- ##### Contact Area Start ##### -->
-
-    <!-- ##### Contact Area End ##### -->
 
     <!-- ##### Footer Area Start ##### -->
 <?php require_once 'layouts/footer.php'; ?>
